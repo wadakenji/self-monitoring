97e0756a6a275da896a1fcb581fc7369e43249fb
wadakenji <ponporopen@gmail.com>
2022-08-14 17:27:32 +0900

    feat: Appコンポーネント更新
    antdのスタイル適用
    ルーティング
    等

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,31 +1,14 @@
-import { GASClient } from 'gas-client'
-import * as server from '../server/main'
-import { useEffect, useState } from 'react'
-
-const { serverFunctions } = new GASClient<typeof server>()
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+import PageTraining from './pages/training'
+import PageIndex from './pages'
 
 const App = () => {
-  const [records, setRecords] = useState<any[][]>([])
-
-  useEffect(() => {
-    const fetch = async () => {
-      await serverFunctions
-        .getTrainingRecords()
-        .then(r => {
-          setRecords(r)
-        })
-        .catch(e => console.error(e))
-    }
-    fetch()
-  }, [])
-
   return (
-    <div>
-      {records.length === 0 && 'loading...'}
-      {records.map(r => (
-        <p key={String(r[0])}>{r.join()}</p>
-      ))}
-    </div>
+    <Routes>
+      <Route path="/" element={<PageIndex />} />
+      <Route path="/training" element={<PageTraining />} />
+    </Routes>
   )
 }
 
