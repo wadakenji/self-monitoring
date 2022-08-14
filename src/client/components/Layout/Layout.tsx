712cc927963f3c5a171afb7432151bdf7b94ed50
wadakenji <ponporopen@gmail.com>
2022-08-14 17:13:43 +0900

    feat: Layoutコンポーネント追加

---
+++ src/client/components/Layout/Layout.tsx
@@ -0,0 +1,35 @@
+import React, { PropsWithChildren } from 'react'
+import { Layout as AntdLayout, Menu } from 'antd'
+import { useLocation, useNavigate } from 'react-router-dom'
+
+const { Header, Content } = AntdLayout
+
+const MENU_ITEMS = [{ key: 'training', label: '筋トレ' }]
+
+const Layout: React.FC<PropsWithChildren> = ({ children }) => {
+  const { pathname } = useLocation()
+  const navigate = useNavigate()
+
+  const selectedKey = pathname.split('/')[1]
+
+  return (
+    <AntdLayout>
+      <Header style={{ display: 'flex' }}>
+        <div style={{ color: 'white', fontSize: 20, marginRight: 20, flexShrink: 0 }} onClick={() => navigate('/')}>
+          わだ日記
+        </div>
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          items={MENU_ITEMS}
+          onClick={({ key }) => navigate(`/${key}`)}
+          selectedKeys={[selectedKey]}
+          style={{ width: '100%' }}
+        />
+      </Header>
+      <Content style={{ padding: '20px 40px 0 40px', minHeight: 280 }}>{children}</Content>
+    </AntdLayout>
+  )
+}
+
+export default Layout
